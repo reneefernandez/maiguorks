7edf93427b52b8322dab4617110f57402ca10f88
Emilia Fernandez <mariaemiliareneefernandez@gmail.com>
2018-05-28 17:31:54 -0300

    modificacion descripcion de peliculas

--- src/pages/descripcion/descripcion.ts
+++ src/pages/descripcion/descripcion.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, NavParams } from 'ionic-angular';
 
 @IonicPage({
   name: 'page-descripcion',
@@ -11,7 +11,15 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class DescripcionPage {
 
-  constructor(public navCtrl: NavController) {
+  public pelicula: any; 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.pelicula = this.navParams.get('pelicula');
   }
+
+
+  public cerrar(): void {
+    this.navCtrl.pop();
+  }
+
   
 }
